e5c105c2d7108dc4d9252f365f58e54906227965
iriszz-official <aiman.iriszz@gmail.com>
2022-05-30 12:51:04 +0800

    Update

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ sleep 1
 sed -i '/^\*\ *soft\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
 sed -i '/^\*\ *hard\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
 echo -e "* soft nofile 65536
-* hard nofile 65536" >>/etc/security/limits.conf
+* hard nofile 65536" >> /etc/security/limits.conf
 locale-gen en_US > /dev/null 2>&1
 
 # Change timezone
